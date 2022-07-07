fe8b67fef1ce297754403053947cfb349bfcb341
Tomasz Strzeszewski <tomaszstrzeszewski@Tomasz-Strzeszewski.local>
2022-07-07 13:56:18 +0200

    Generated controller static-pages

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
+  config.assets.css_compressor = :yui
+  config.assets.js_compressor = :terser
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
