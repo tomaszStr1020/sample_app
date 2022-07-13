2675a513afa472f4ca17ca1a5aea8873bf74dff9
tomaszstrzeszewski <tomasz.strzeszewski@htdevelopers.com>
2022-07-13 16:57:17 +0200

    Added form for sign up new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+  def show
+    @user = User.find(params[:id])
+    # debugger
+  end
+  def index
+    @user = User.all
+
   end
 end
